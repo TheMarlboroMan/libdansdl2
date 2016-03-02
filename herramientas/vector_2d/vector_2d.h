d7d7677eb9146a33bd9273e7883cdeeed051ddda
Daniel Pastor <daniel.pastor@grupoancana.com>
2016-03-02 14:19:42 +0100

    El vector_2d pasa a ser template.
    La compatibilidad con el código anterior se rompe, por supuesto.

--- herramientas/vector_2d/vector_2d.h
+++ herramientas/vector_2d/vector_2d.h
@@ -2,56 +2,146 @@
 #define VECTOR_2D_H
 
 #include <cmath>
+#include "../herramientas/herramientas.h"
 
 namespace DLibH
 {
 
 /**
-* Un vector usable directamente, agnóstico de su tipo. Simplemente indica una
-* dirección y una velocidad.
+* Vector simple... Para uso normal y corriente.
 */
 
-struct Vector_2d_cartesiano;
-struct Vector_2d_pantalla;
-
+template<typename T>
 struct Vector_2d
 {
-	float x;
-	float y;
+	T x;
+	T y;
 
-	Vector_2d(): x(0.f), y(0.f) {}
-	Vector_2d(const Vector_2d& o): x(o.x), y(o.y) {}
-	Vector_2d(float p_x, float p_y):x(p_x), y(p_y) {}
+	Vector_2d(): x(), y() 
+	{
 
-	Vector_2d operator+(const Vector_2d &otro);
-	Vector_2d operator-(const Vector_2d &otro);
-	Vector_2d operator*(const Vector_2d &otro);
-	Vector_2d operator/(const Vector_2d &otro);
+	}
 
-	Vector_2d& operator=(const Vector_2d &otro);
+	Vector_2d(const Vector_2d<T>& o)
+		: x(o.x), y(o.y) 
+	{
 
-	Vector_2d& operator+=(const Vector_2d &otro);
-	Vector_2d& operator-=(const Vector_2d &otro);
-	Vector_2d& operator*=(const Vector_2d &otro);
-	Vector_2d& operator/=(const Vector_2d &otro);
+	}
 
-	Vector_2d& operator*(const float p_multiplicador);
-	Vector_2d& operator/(const float p_divisor);
-	Vector_2d& operator*=(const float p_multiplicador);
-	Vector_2d& operator/=(const float p_divisor);
+	Vector_2d(T p_x, T p_y)
+		:x(p_x), y(p_y) 
+	{
+	
+	}
 
-	void normalizar();
-	float longitud() const;
-	float angulo_radianes() const;
-	float angulo_grados() const;
+	Vector_2d operator+(const Vector_2d<T> &otro)
+	{
+		return Vector_2d(this->x+otro.x, this->y+otro.y);
+	}
 
-	static float obtener_angulo_para_vector_unidad_radianes(const Vector_2d&);
-	static float obtener_angulo_para_vector_unidad_grados(const Vector_2d&);
+	Vector_2d operator-(const Vector_2d<T> &otro)
+	{
+		return Vector_2d(this->x-otro.x, this->y-otro.y);
+	}
 
-	static Vector_2d vector_unidad_para_angulo(float);
+	Vector_2d operator*(const Vector_2d<T> &otro)
+	{
+		return Vector_2d(this->x*otro.x, this->y*otro.y);
+	}
 
-	static Vector_2d_cartesiano obtener_para_puntos_cartesiano(float p_xa, float p_ya, float p_xb, float p_yb);
-	static Vector_2d_pantalla obtener_para_puntos_pantalla(float p_xa, float p_ya, float p_xb, float p_yb);
+	Vector_2d operator/(const Vector_2d<T> &otro)
+	{
+		return Vector_2d(this->x/otro.x, this->y/otro.y);
+	}
+
+	Vector_2d& operator=(const Vector_2d<T> &otro)
+	{
+		this->x=otro.x;
+		this->y=otro.y;
+		return *this;
+	}
+
+	Vector_2d& operator+=(const Vector_2d<T> &otro)
+	{
+		this->x+=otro.x;
+		this->y+=otro.y;
+		return *this;
+	}
+
+	Vector_2d& operator-=(const Vector_2d<T> &otro)
+	{
+		this->x-=otro.x;
+		this->y-=otro.y;
+		return *this;
+	}
+
+	Vector_2d& operator*=(const Vector_2d<T> &otro)
+	{
+		this->x*=otro.x;
+		this->y*=otro.y;
+		return *this;
+	}
+
+	Vector_2d& operator/=(const Vector_2d<T> &otro)
+	{
+		this->x/=otro.x;
+		this->y/=otro.y;
+		return *this;
+	}
+
+	Vector_2d& operator*(const T p_multiplicador)
+	{
+		this->x=this->x*p_multiplicador;
+		this->y=this->y*p_multiplicador;
+		return *this;
+	}
+
+	Vector_2d& operator/(const T p_divisor)
+	{
+		this->x=this->x/p_divisor;
+		this->y=this->y/p_divisor;
+		return *this;
+	}
+
+	Vector_2d& operator*=(const T p_multiplicador)
+	{
+		this->x*=p_multiplicador;
+		this->y*=p_multiplicador;
+		return *this;
+	}
+
+	Vector_2d& operator/=(const T p_divisor)
+	{
+		this->x/=p_divisor;
+		this->y/=p_divisor;
+		return *this;
+	}
+
+	void normalizar()
+	{
+		T m=this->longitud();
+		this->x=this->x / m;
+		this->y=this->y / m;
+	}
+
+	T longitud() const
+	{
+		return sqrtf(this->x*this->x + this->y*this->y);
+	}
+
+	T angulo_radianes() const
+	{
+		auto vec(*this);
+		vec.normalizar();
+		return obtener_angulo_para_vector_unidad_radianes(vec);
+	}
+
+	T angulo_grados() const
+	{
+		auto vec(*this);
+		vec.normalizar();
+		return obtener_angulo_para_vector_unidad_grados(vec);
+	}
 };
 
 /**
@@ -60,52 +150,274 @@ struct Vector_2d
 * haya confusiones luego en el código cliente.
 */
 
+template<typename T>
 struct Vector_2d_pantalla:
-	public Vector_2d
+	public Vector_2d<T>
 {
-						Vector_2d_pantalla(float px, float py):Vector_2d(px, py) {}
-						Vector_2d_pantalla(const Vector_2d& v);
-						Vector_2d_pantalla& operator=(const Vector_2d& v);
-	Vector_2d_cartesiano			a_cartesiano() const;
-	Vector_2d_pantalla 			operator+(const Vector_2d &otro);
-	Vector_2d_pantalla 			operator-(const Vector_2d &otro);
-	Vector_2d_pantalla 			operator*(const Vector_2d &otro);
-	Vector_2d_pantalla 			operator/(const Vector_2d &otro);
-	Vector_2d_pantalla& 			operator+=(const Vector_2d &otro);
-	Vector_2d_pantalla& 			operator-=(const Vector_2d &otro);
-	Vector_2d_pantalla& 			operator*=(const Vector_2d &otro);
-	Vector_2d_pantalla& 			operator/=(const Vector_2d &otro);
-	Vector_2d_pantalla&			operator*(const float p_multiplicador);
-	Vector_2d_pantalla& 			operator/(const float p_divisor);
-	Vector_2d_pantalla& 			operator*=(const float p_multiplicador);
-	Vector_2d_pantalla& 			operator/=(const float p_divisor);	
+						Vector_2d_pantalla(T px, T py):Vector_2d<T>(px, py) 
+	{
+
+	}
+
+						Vector_2d_pantalla(const Vector_2d_pantalla<T>& v)
+	{
+		this->x=v.x;
+		this->y=v.x;
+	}
+
+						Vector_2d_pantalla& operator=(const Vector_2d_pantalla<T>& v)
+	{
+		Vector_2d<T>::operator=(v);
+		return *this;
+	}
+
+	Vector_2d_pantalla 			operator+(const Vector_2d_pantalla<T> &otro)
+	{
+		return Vector_2d_pantalla(this->x+otro.x, this->y+otro.y);
+	}
+
+	Vector_2d_pantalla 			operator-(const Vector_2d_pantalla<T> &otro)
+	{
+		return Vector_2d_pantalla(this->x-otro.x, this->y-otro.y);
+	}
+
+	Vector_2d_pantalla 			operator*(const Vector_2d_pantalla<T> &otro)
+	{
+		return Vector_2d_pantalla(this->x*otro.x, this->y*otro.y);
+	}
+
+	Vector_2d_pantalla 			operator/(const Vector_2d_pantalla<T> &otro)
+	{
+		return Vector_2d_pantalla(this->x/otro.x, this->y/otro.y);
+	}
+
+	Vector_2d_pantalla& 			operator+=(const Vector_2d_pantalla<T> &otro)
+	{
+		this->x+=otro.x;
+		this->y+=otro.y;
+		return *this;
+	}
+
+	Vector_2d_pantalla& 			operator-=(const Vector_2d_pantalla<T> &otro)
+	{
+		this->x-=otro.x;
+		this->y-=otro.y;
+		return *this;
+	}
+
+	Vector_2d_pantalla& 			operator*=(const Vector_2d_pantalla<T> &otro)
+	{
+		this->x*=otro.x;
+		this->y*=otro.y;
+		return *this;
+	}
+
+	Vector_2d_pantalla& 			operator/=(const Vector_2d_pantalla<T> &otro)
+	{
+		this->x/=otro.x;
+		this->y/=otro.y;
+		return *this;
+	}
+
+	Vector_2d_pantalla&			operator*(const T p_multiplicador)
+	{
+		this->x=this->x*p_multiplicador;
+		this->y=this->y*p_multiplicador;
+		return *this;
+	}
+
+	Vector_2d_pantalla& 			operator/(const T p_divisor)
+	{
+		this->x=this->x/p_divisor;
+		this->y=this->y/p_divisor;
+		return *this;
+	}
+
+	Vector_2d_pantalla& 			operator*=(const T p_multiplicador)
+	{
+		this->x*=p_multiplicador;
+		this->y*=p_multiplicador;
+		return *this;
+	}
+
+	Vector_2d_pantalla& 			operator/=(const T p_divisor)
+	{
+		this->x/=p_divisor;
+		this->y/=p_divisor;
+		return *this;
+	}
 };
 
 /**
 * Este vector es cartesiano: y positivo es arriba, y negativo es abajo.
 */
 
+template<typename T>
 struct Vector_2d_cartesiano:
-	public Vector_2d
+	public Vector_2d<T>
 {
-						Vector_2d_cartesiano(float px, float py):Vector_2d(px, py) {}
-						Vector_2d_cartesiano(const Vector_2d& v);
-						Vector_2d_cartesiano& operator=(const Vector_2d& v);
-	Vector_2d_pantalla			a_pantalla() const;
-	Vector_2d_cartesiano 			operator+(const Vector_2d &otro);
-	Vector_2d_cartesiano 			operator-(const Vector_2d &otro);
-	Vector_2d_cartesiano 			operator*(const Vector_2d &otro);
-	Vector_2d_cartesiano 			operator/(const Vector_2d &otro);
-	Vector_2d_cartesiano& 			operator+=(const Vector_2d &otro);
-	Vector_2d_cartesiano& 			operator-=(const Vector_2d &otro);
-	Vector_2d_cartesiano& 			operator*=(const Vector_2d &otro);
-	Vector_2d_cartesiano& 			operator/=(const Vector_2d &otro);
-	Vector_2d_cartesiano&			operator*(const float p_multiplicador);
-	Vector_2d_cartesiano& 			operator/(const float p_divisor);
-	Vector_2d_cartesiano& 			operator*=(const float p_multiplicador);
-	Vector_2d_cartesiano& 			operator/=(const float p_divisor);
-	
+						Vector_2d_cartesiano(T px, T py):Vector_2d<T>(px, py) 
+	{
+
+	}
+
+						Vector_2d_cartesiano(const Vector_2d_cartesiano<T>& v)
+	{
+		this->x=v.x;
+		this->y=v.y;
+	}
+
+						Vector_2d_cartesiano& operator=(const Vector_2d_cartesiano<T>& v)
+	{
+		Vector_2d<T>::operator=(v);
+		return *this;
+	}
+
+	Vector_2d_cartesiano 			operator+(const Vector_2d_cartesiano<T> &otro)
+	{
+		return Vector_2d_cartesiano(this->x+otro.x, this->y+otro.y);
+	}
+
+	Vector_2d_cartesiano 			operator-(const Vector_2d_cartesiano<T> &otro)
+	{
+		return Vector_2d_cartesiano(this->x-otro.x, this->y-otro.y);
+	}
+
+	Vector_2d_cartesiano 			operator*(const Vector_2d_cartesiano<T> &otro)
+	{
+		return Vector_2d_cartesiano(this->x*otro.x, this->y*otro.y);
+	}
+
+	Vector_2d_cartesiano 			operator/(const Vector_2d_cartesiano<T> &otro)
+	{
+		return Vector_2d_cartesiano(this->x/otro.x, this->y/otro.y);
+	}
+
+	Vector_2d_cartesiano& 			operator+=(const Vector_2d_cartesiano<T> &otro)
+	{
+		this->x+=otro.x;
+		this->y+=otro.y;
+		return *this;
+	}
+
+	Vector_2d_cartesiano& 			operator-=(const Vector_2d_cartesiano<T> &otro)
+	{
+		this->x-=otro.x;
+		this->y-=otro.y;
+		return *this;
+	}
+
+	Vector_2d_cartesiano& 			operator*=(const Vector_2d_cartesiano<T> &otro)
+	{
+		this->x*=otro.x;
+		this->y*=otro.y;
+		return *this;
+	}
+
+	Vector_2d_cartesiano& 			operator/=(const Vector_2d_cartesiano<T> &otro)
+	{
+		this->x/=otro.x;
+		this->y/=otro.y;
+		return *this;
+	}
+
+	Vector_2d_cartesiano&			operator*(const T p_multiplicador)
+	{
+		this->x=this->x*p_multiplicador;
+		this->y=this->y*p_multiplicador;
+		return *this;
+	}
+
+	Vector_2d_cartesiano& 			operator/(const T p_divisor)
+	{
+		this->x=this->x/p_divisor;
+		this->y=this->y/p_divisor;
+		return *this;
+	}
+
+	Vector_2d_cartesiano& 			operator*=(const T p_multiplicador)
+	{
+		this->x*=p_multiplicador;
+		this->y*=p_multiplicador;
+		return *this;
+	}
+
+	Vector_2d_cartesiano& 			operator/=(const T p_divisor)
+	{
+		this->x/=p_divisor;
+		this->y/=p_divisor;
+		return *this;
+	}
 };
+
+/*El orden de los factores PUEDE alterar el producto.*/
+template<typename T>
+Vector_2d_pantalla<T> obtener_para_puntos_pantalla(T p_xa, T p_ya, T p_xb, T p_yb)
+{
+	Vector_2d_pantalla<T> r;
+
+	if(! (p_xa==p_xb && p_ya==p_yb))
+	{
+		r.x=p_xa-p_xb;
+		r.y=p_ya-p_yb;
+		r.normalizar();
+	}
+
+	return r;
+}
+
+template<typename T>
+Vector_2d_cartesiano<T> obtener_para_puntos_cartesiano(T p_xa, T p_ya, T p_xb, T p_yb)
+{
+	Vector_2d_cartesiano<T> r;
+
+	if(! (p_xa==p_xb && p_ya==p_yb))
+	{
+		r.x=p_xa-p_xb;
+		r.y=p_yb-p_ya;
+		r.normalizar();
+	}
+
+	return r;
+}
+
+template<typename T>
+Vector_2d_cartesiano<T>	a_cartesiano(const Vector_2d_pantalla<T>& v)
+{
+	return Vector_2d_cartesiano<T>(v.x, -v.y);
+}
+
+template<typename T>
+Vector_2d_pantalla<T> a_pantalla(const Vector_2d_cartesiano<T>& v)
+{
+	return Vector_2d_pantalla<T>(v.x, -v.y);
+}
+
+template<typename T>
+T obtener_angulo_para_vector_unidad_radianes(const Vector_2d<T>& p_vector)
+{
+	T rad=std::atan2(p_vector.y, p_vector.x);
+	return rad;
+}
+
+template<typename T>
+T obtener_angulo_para_vector_unidad_grados(const Vector_2d<T>& p_vector)
+{
+	T rad=obtener_angulo_para_vector_unidad_radianes(p_vector);
+	T grados=(rad / M_PI) * 180.0;
+	return grados;
+}
+
+template<typename T>
+Vector_2d<T> vector_unidad_para_angulo(T p_angulo)
+{
+	T rad=DLibH::Herramientas::grados_a_radianes(p_angulo);
+	T v_x=sin(rad);
+	T v_y=cos(rad);
+
+	Vector_2d<T> r(v_x, v_y);
+	return r;
+}
 
 } //Fin namespace DLibH
 #endif
