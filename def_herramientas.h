09648f6b8fb326cd001b835288b6ef482a5c3274
Dani <marlborometal@gmail.com>
2016-07-14 19:14:34 +0200

    Removed all map and template managers

--- def_herramientas.h
+++ def_herramientas.h
@@ -8,10 +8,6 @@
 #include "tools/caja/caja.h"
 #include "tools/vector_2d/vector_2d.h"
 #include "tools/punto_2d/punto_2d.h"
-#include "tools/templates/gestor_vector_instanciable.h"
-#include "tools/templates/gestor_vector_static.h"
-#include "tools/templates/gestor_mapa_instanciable.h"
-#include "tools/templates/gestor_mapa_static.h"
 #include "tools/controlador_fps_sdl/controlador_fps_sdl.h"
 #include "tools/tools/tools.h"
 
