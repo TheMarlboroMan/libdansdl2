eaef52d4b9713c779c6586bc9c23afa45a1998c0
Dani <marlborometal@gmail.com>
2016-07-14 19:20:54 +0200

    Translated Punto_2d

--- def_herramientas.h
+++ def_herramientas.h
@@ -7,7 +7,7 @@
 #include "tools/poligono_2d/poligono_2d.h"
 #include "tools/caja/caja.h"
 #include "tools/vector_2d/vector_2d.h"
-#include "tools/punto_2d/punto_2d.h"
+#include "tools/point_2d/point_2d.h"
 #include "tools/controlador_fps_sdl/controlador_fps_sdl.h"
 #include "tools/tools/tools.h"
 
