54adc8e1973895185559a8eb47bba1bb6aee59de
TheMarlboroMan <marlborometal@gmail.com>
2016-07-21 00:28:02 +0200

    Small fix

--- video/representation/raster/bitmap/bitmap_representation.h
+++ video/representation/raster/bitmap/bitmap_representation.h
@@ -16,7 +16,8 @@ class bitmap_representation:
 			bitmap_representation(const bitmap_representation&);
 			bitmap_representation& operator=(const bitmap_representation &);
 
-	void 		set_texture_skip_clip(texture const * r){raster_representation::set_texture(r);}
+	//TODO: What kind of name is this???
+	void 		set_texture_skip_clip(const texture& r){raster_representation::set_texture(r);}
 };
 
 } //Fin namespace DLibV
