901c2b12ee9cf478a26d0f0f33e481f2e6d11e98
TheMarlboroMan <marlborometal@gmail.com>
2017-08-05 12:11:27 +0200

    Remove commented functions

--- video/texture/texture.cpp
+++ video/texture/texture.cpp
@@ -16,52 +16,6 @@ texture::~texture()
 	glDeleteTextures(1, &index);
 }
 
-/*
-texture::texture(const texture& t)
-	:index(0), mode(t.mode), w(t.w), h(t.h)
-{
-
-	//TODO: This isn't working.
-	glBindTexture(GL_TEXTURE_2D, t.index);
-	glCopyTexImage2D(GL_TEXTURE_2D, 0, mode, 0, 0, w, h, 0);
-	glPixelStorei(GL_PACK_ALIGNMENT, 1);
-	std::vector<uint8_t> raw_img;
-
-	//TODO: Test this with other sizes.
-	int bpp=mode==GL_RGBA ? 4 : 3;
-	raw_img.reserve((w*h)*bpp);
-
-	glGetTexImage(GL_TEXTURE_2D, 0, mode, GL_UNSIGNED_BYTE, raw_img.data());
-	glGenTextures(1, &index);
-	glBindTexture(GL_TEXTURE_2D, index);
-	glTexImage2D(GL_TEXTURE_2D, 0, mode, w, h, 0, mode, GL_UNSIGNED_BYTE, raw_img.data());
-}
-
-texture& texture::operator=(const texture& t)
-{
-	//TODO: This isn't working.
-	mode=t.mode;
-	w=t.w;
-	h=t.h;
-
-	glBindTexture(GL_TEXTURE_2D, t.index);
-	glCopyTexImage2D(GL_TEXTURE_2D, 0, mode, 0, 0, w, h, 0);
-	glPixelStorei(GL_PACK_ALIGNMENT, 1);
-	std::vector<uint8_t> raw_img;
-
-	//TODO: Test this with other sizes.
-	int bpp=mode==GL_RGBA ? 4 : 3;
-	raw_img.reserve((w*h)*bpp);
-
-	glGetTexImage(GL_TEXTURE_2D, 0, mode, GL_UNSIGNED_BYTE, raw_img.data());
-	glGenTextures(1, &index);
-	glBindTexture(GL_TEXTURE_2D, index);
-	glTexImage2D(GL_TEXTURE_2D, 0, mode, w, h, 0, mode, GL_UNSIGNED_BYTE, raw_img.data());
-
-	return *this;
-}
-*/
-
 void texture::replace(const surface& s)
 {
 	load(s.get_surface());
